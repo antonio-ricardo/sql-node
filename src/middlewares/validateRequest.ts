62c26acc1950e4919373b15235a2f4c4f021ce92
antonio ricardo <antonio.gugou@gmail.com>
2022-08-31 14:34:55 -0300

    creating validateRequestMiddleware

---
+++ src/middlewares/validateRequest.ts
@@ -0,0 +1,20 @@
+import { NextFunction, Request, Response } from "express";
+
+export const validateRequest = (
+  req: any,
+  res: Response,
+  next: NextFunction,
+  dto: any
+) => {
+  req.body = {
+    ...req.body,
+    ...req.headers,
+    ...req.params,
+    ...req.query,
+  };
+
+  const validateBody = dto.validate(req.body);
+  req.body = validateBody;
+
+  next();
+};
