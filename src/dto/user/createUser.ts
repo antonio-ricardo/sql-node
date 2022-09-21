28771fb637a5de003c975bfc16e1e94373c722f4
antonio ricardo <antonio.gugou@gmail.com>
2022-09-21 19:28:46 -0300

    changin create user logic

--- src/dto/user/createUser.ts
+++ src/dto/user/createUser.ts
@@ -3,21 +3,27 @@ import { object, string } from "yup";
 const createUserSchema = object({
   name: string().required(),
   email: string().required(),
+  password: string().required(),
 });
 
 interface UserData {
   name: string;
   email: string;
+  password: string;
 }
 
 export class CreateUserDto {
-  constructor(public name: string, public email: string) {}
+  constructor(
+    public name: string,
+    public email: string,
+    public password: string
+  ) {}
 
   static validate(data: Partial<UserData>) {
-    const { name, email } = createUserSchema
+    const { name, email, password } = createUserSchema
       .camelCase()
       .validateSync(data, { stripUnknown: true });
 
-    return new CreateUserDto(name, email);
+    return new CreateUserDto(name, email, password);
   }
 }
--- src/services/user/create.ts
+++ src/services/user/create.ts
@@ -1,6 +1,7 @@
 import { prisma } from "../../database/connection";
 import { ConflictError } from "../../common/errors";
 import { CreateUserDto } from "../../dto";
+import { hash } from "bcrypt";
 
 export const createUserService = async (input: CreateUserDto) => {
   const userExists = await prisma.user.findFirst({
@@ -9,12 +10,14 @@ export const createUserService = async (input: CreateUserDto) => {
     },
   });
 
+  const hashedPassword = await hash(input.password, 10);
+
   if (userExists) {
     throw new ConflictError("User already exists");
   }
 
   const createdUser = await prisma.user.create({
-    data: input,
+    data: { ...input, password: hashedPassword },
   });
 
   return createdUser;
