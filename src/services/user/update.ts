c2b28777907eb6042d59c0d34bdc165c99a985f8
antonio ricardo <antonio.gugou@gmail.com>
2022-09-21 19:29:04 -0300

    changin update logic

--- src/services/user/update.ts
+++ src/services/user/update.ts
@@ -1,20 +1,31 @@
 import { prisma } from "./../../database/connection";
-import { CreateUserDto } from "../../dto";
-import { NotFoundError } from "../../common/errors";
+import { UpdateUserDto } from "../../dto";
+import { ConflictError, NotFoundError } from "../../common/errors";
+import { hash } from "bcrypt";
 
-export const updateUserService = async (input: CreateUserDto) => {
+export const updateUserService = async (input: UpdateUserDto) => {
+  if (!input.toUpdate.name && input.toUpdate.hasDeleted == undefined) {
+    throw new ConflictError("Any data sent to update the user");
+  }
+
+  const userExists = await prisma.user.findFirst({
+    where: { email: input.email },
+  });
+
+  if (!userExists) {
+    throw new NotFoundError("Not find any user with sent email");
+  }
+
+  if (input.toUpdate.password) {
+    input.toUpdate.password = await hash(input.toUpdate.password, 10);
+  }
+
   const user = await prisma.user.update({
     where: {
       email: input.email,
     },
-    data: {
-      name: input.name,
-    },
+    data: input.toUpdate,
   });
-
-  if (!user) {
-    throw new NotFoundError("Not find any user with sent email");
-  }
 
   return user;
 };
