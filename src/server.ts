9035cfa9af2407471fbbe6f892b5811230af228a
antonio ricardo <antonio.gugou@gmail.com>
2022-09-25 19:31:40 -0300

    add cors in server.ts

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,11 @@ import express from 'express';
 import { errorHandler } from './middlewares/errorHandler';
 import userRoutes from './routes/userRoutes';
 import transactionRoutes from './routes/transactionRoutes';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use('/users', userRoutes);
 app.use('/transactions', transactionRoutes);
